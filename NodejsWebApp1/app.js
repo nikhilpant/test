4cf654900bd5085e79e336b8167f7dfd46042baf
nikhilpant <nikhil.pant@gmail.com>
2017-01-30 14:08:13 -0800

    Adding underscore and code to return persons and persons/:personId

--- NodejsWebApp1/app.js
+++ NodejsWebApp1/app.js
@@ -1,6 +1,22 @@
-﻿// Load modules
+﻿var personData = [
+    {
+        "id": 1,
+        "firstName": "Ted",
+        "lastName": "Neward",
+        "status": "MEANing"
+    },
+    {
+        "id": 2,
+        "firstName": "Brian",
+        "lastName": "Randell",
+        "status": "TFSing"
+    }
+];
+
+// Load modules
 var express = require('express');
 var debug = require('debug')('app');
+var _ = require('underscore');
 // Create express instance
 var app = express();
 // Set up a simple route
@@ -8,6 +24,36 @@ app.get('/', function (req, res) {
     debug("/ requested");
     res.send('Hello World! express');
 });
+// Setup persons
+var getAllPersons = function (req, res) {
+    debug("getAllPersons Called:");
+    var response = personData;
+    res.send(JSON.stringify(response));
+};
+app.get('/persons', getAllPersons);
+
+var getPerson = function (req, res) {
+    debug("getPersonCalled:", req.person);
+    if (req.person) {
+        res.send(200, JSON.stringify(req.person));
+    }
+    else {
+        res.send(400, { message: "Unrecognized identifier: " + identifier });
+    }
+};
+
+// get person by id
+app.get('/persons/:personId', getPerson);
+app.param('personId', function (req, res, next, personId) {
+    debug("personId found:", personId);
+    var person = _.find(personData, function (it) {
+        return personId == it.id;
+    });
+    debug("person:", person);
+    req.person = person;
+    next();
+});
+
 // Start the server
 var port = process.env.PORT || 3000;
 debug("We picked up", port, "for the port");
--- NodejsWebApp1/typings/index.d.ts
+++ NodejsWebApp1/typings/index.d.ts
@@ -1,1 +1,2 @@
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/underscore/index.d.ts" />
